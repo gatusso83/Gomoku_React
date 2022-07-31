f59ec663344b86677dcdeef4db1fc2a8833359e1
Mark <markgatus@hotmail.com>
2022-07-31 21:47:26 +1000

    Login page complete, valid credential hook added, Message component created and added to login page for incorrect login credentials

--- gomoku/src/components/index.ts
+++ gomoku/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button'
 export { default as Input } from './Input'
 export { default as Dropdown } from './DropDown'
+export { default as Message } from './Message'
--- gomoku/src/pages/Login.tsx
+++ gomoku/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-import { Button, Input } from '../components'
+import { Button, Input, Message } from '../components'
+import users from '../data/users.json'
 
 
 import style from './Login.module.css'
@@ -8,11 +9,35 @@ import style from './Login.module.css'
 export default function Login() {
     const [username, setUserName] = useState('')
     const [password, setPassword] = useState('')
+    const [isCredentialInvalid, setIsCredentialInvalid] = useState(false)
+
+    const handleLogin = () => {
+        const user = users.find((u) => u.username === username && u.password === password)
+        if (!user) {
+            setIsCredentialInvalid(true)
+        } else {
+            console.log("logged in")
+        }
+    }
 
     return (
-        <form className={style.container}>
-            <Input name="username" placeholder="Username" value={username} onChange={(e) => setUserName(e.target.value)} />
-            <Input name="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <form className={style.container} onSubmit={(e) => {
+            e.preventDefault()
+            handleLogin()
+        }}>
+            {isCredentialInvalid && (<Message variant="error" message="Invalid username or password" />)}
+            <Input name="username" placeholder="Username" value={username}
+                onChange={(e) => {
+                    setUserName(e.target.value)
+                    setIsCredentialInvalid(false)
+                }}
+            />
+            <Input name="password" type="password" placeholder="Password" value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                    setIsCredentialInvalid(false)
+                }}
+            />
             <Button type="submit">Login</Button>
         </form >
     )
